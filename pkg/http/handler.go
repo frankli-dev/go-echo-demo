cb2bb03b22611163d37ebca99041179a246a5ddc
frankli <frankli.consult@gmail.com>
2020-03-06 23:27:35 -0400

    REF: getRenewableResources handler update
    update handler to return an html template.

--- pkg/http/handler.go
+++ pkg/http/handler.go
@@ -4,22 +4,20 @@ import (
 	"net/http"
 
 	"github.com/ivan-marquez/golang-demo/pkg/listing"
-
 	"github.com/labstack/echo/v4"
 )
 
 // TODO: add comment
-func Handler(e *echo.Echo, s listing.Service) {
-	e.GET("/renewables", getRenewableResources(s))
+func getRenewableResources(s listing.Service) func(c echo.Context) error {
+	return func(c echo.Context) error {
+		c.Response().Header().Set("Content-Type", "text/html")
+		// TODO: add error handling
+		res := s.GetRenewableResources()
+		return c.Render(http.StatusOK, "renewables", res)
+	}
 }
 
 // TODO: add comment
-func getRenewableResources(s listing.Service) func(c echo.Context) error {
-	return func(c echo.Context) error {
-		c.Response().Header().Set("Content-Type", "application/json")
-		// TODO: add error handling
-		res := s.GetRenewableResources()
-
-		return c.JSON(http.StatusOK, res)
-	}
+func Handler(e *echo.Echo, s listing.Service) {
+	e.GET("/renewables", getRenewableResources(s))
 }
